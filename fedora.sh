e0eabb1a3928c779456c489f26c424f2aabeb32e
arg03 <guptaarnav376@gmail.com>
2023-01-29 02:37:58 +0530

    fedora.sh

---
+++ fedora.sh
@@ -0,0 +1,18 @@
+#This is a list of commands that I usually run after installing Fedora
+#adding repos
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo dnf groupupdate core
+#codecs
+sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+sudo dnf install lame\* --exclude=lame-devel
+sudo dnf group upgrade --with-optional Multimedia
+#Remove Firefox
+sudo dnf remove firefox
+#Brave Browser
+sudo dnf install dnf-plugins-core
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install brave-browser
+#Packages to install
+sudo dnf install neovim fish neofetch qbittorrent mpv gnome-tweaks yt-dlp
