78f7ab1876890caf16e2769373a21e64b5d42632
Topmay <101392661+Topmay@users.noreply.github.com>
2023-04-29 14:46:35 +0530

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -6,12 +6,5 @@ sudo dnf groupupdate core
 sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
 sudo dnf install lame\* --exclude=lame-devel
 sudo dnf group upgrade --with-optional Multimedia
-#Remove Firefox
-sudo dnf remove firefox
-#Brave Browser
-sudo dnf install dnf-plugins-core
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser
 #Packages to install
-sudo dnf install zsh neovim qbittorrent mpv gnome-tweaks yt-dlp btop vlc 
+sudo dnf install zsh neovim qbittorrent mpv gnome-tweaks yt-dlp btop vlc exa
